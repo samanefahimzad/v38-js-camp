eff52271e6e9e5d38ad62c539847e72d917b9228
samane fahimzad <samanefahimzad@gmail.com>
2026-09-16 12:55:57 +0200

    feat: named function with return and call

--- script.js
+++ script.js
@@ -8,4 +8,9 @@ console.log(ticketCount);
 const isVip = false;
 console.log(isVip);
 
+function totalMinutes (hours, minutes){
+return hours * 60 + minutes;
+}
+const travel = totalMinutes (2, 15);
+console.log(totalMinutes);
  
