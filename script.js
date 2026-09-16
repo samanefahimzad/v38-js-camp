0b6f82f81c2558640a546653a44330518efde35c
samane fahimzad <samanefahimzad@gmail.com>
2026-09-16 12:35:05 +0200

    feat: introduce const/let and basic types

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-console.log("Nord Glow");
-console.log("Nord Glow");
-console.log("Nord Glow");
+const eventName = "Samane Live"
+console.log(eventName);
+console.log(eventName);
+let ticketCount = 0;
+console.log(ticketCount);
+ticketCount = 10;
+console.log(ticketCount);
+const isVip = false;
+console.log(isVip);
+
+ 
